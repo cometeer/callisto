a54da5d2f6fa29fb117e92a3b14bba6723687949
Tristan Geraets <tristang@protonmail.ch>
2015-09-15 17:27:42 +0200

    small tweaks on the contractor form

--- client/template/contractor/contractorCreate.js
+++ client/template/contractor/contractorCreate.js
@@ -3,16 +3,21 @@ Template.contractorCreate.events({
     e.preventDefault();
   },
   'click .addOption': function(e) {
+    // get the set of options
     var options = Session.get('configOptions');
+    // if there where no options, create empty array, and load it right away.
     if (typeof options === 'undefined') {
       Session.set('configOptions', []);
+      options = Session.get('configOptions');
     }
+    // create a new option with the values of the fields
     var option = {};
-    var name = $('#newConfigOptionName').val();
-    var value = $('#newConfigOptionValue').val();
-    option.name = name;
-    option.value = value;
-    console.log(options);
+    option.name = $('#newConfigOptionName').val();
+    option.value = $('#newConfigOptionValue').val();
+    // clear the fields for a new entry
+    document.getElementById("newConfigOptionName").value = '';
+    document.getElementById("newConfigOptionValue").value = '';
+    // push and save it in session
     options.push(option);
     Session.set('configOptions', options);
   },
